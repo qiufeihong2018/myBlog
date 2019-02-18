622e35ac04fb49ce86b2bc372617fa852f8f7eae
qiufeihong <qiufeihong2018@126.com>
2019-02-18 11:31:14 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env sh
+
 # 确保脚本抛出遇到的错误
 set -e
+
 # 生成静态文件
-npm run docs:build
+npm run build
+
 # 进入生成的文件夹
 cd docs/.vuepress/dist
+
 # 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+# echo 'www.yourwebsite.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
-# 如果发布到 https://<USERNAME>.github.io  USERNAME=你的用户名
+
+# 如果你想要部署到 https://USERNAME.github.io
 git push -f git@github.com:qiufeihong2018/qiufeihong2018.github.io.git master
-# 如果发布到 https://<USERNAME>.github.io/<REPO>  REPO=github上的项目
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+# 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
+# git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
+
 cd -
