aaa508f363497d78ac67192b732371c1b12a0d37
qiufeihong <qiufeihong2018@126.com>
2019-02-19 15:02:22 +0800

    最大搜索数量

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,7 @@ module.exports = {
         ['link', {rel: 'icon', href: '/logo.gif'}]
     ],
     markdown: {
-        lineNumbers: true // 代码块显示行号
+        lineNumbers: true
     },
     themeConfig: {
         nav: [
@@ -87,7 +87,8 @@ module.exports = {
             }
         ],
         sidebar: 'auto',
-        sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
-        lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
+        sidebarDepth: 2,
+        lastUpdated: 'Last Updated',
+        searchMaxSuggestoins: 10,
     }
 }
