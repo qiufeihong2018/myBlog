790ee7df5be435d7cedb4c4c9c3439f9e0e4aeab
qiufeihong2018 <15058301288@163.com>
2019-02-27 17:02:17 +0800

    add sidebar

---
+++ docs/interview/sidebar.js
@@ -0,0 +1,22 @@
+module.exports = {
+    sidebar: [
+        {
+            title: '面试题',
+            collapsable: false,
+            sidebarDepth: 2,
+            children: ['command/']
+        },
+        {
+            title: '前端面试之道',
+            collapsable: false,
+            sidebarDepth: 2,
+            children: ['opration/']
+        },
+        {
+            title: '慕课网-前端跳槽面试必备技巧',
+            collapsable: false,
+            sidebarDepth: 2,
+            children: ['opration/']
+        }
+    ]
+}
