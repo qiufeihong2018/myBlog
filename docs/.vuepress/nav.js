9b3f67951b129b7560b5edc681a4af68628eb846
qiufeihong <qiufeihong2018@126.com>
2019-03-29 14:04:32 +0800

    edit nav

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -69,82 +69,94 @@ module.exports = [
         text: '官网链接',
         items: [
             {
-                text: 'MDN Web 文档', link: 'https://developer.mozilla.org/zh-CN/'
+                text: '官方文档',
+                items: [
+                    {
+                        text: 'npm 中文文档', link: 'https://www.npmjs.cn/'
+                    },
+                    {
+                        text: 'Vue.js', link: 'https://cn.vuejs.org/'
+                    },
+                    {
+                        text: 'Nuxt.js', link: 'https://zh.nuxtjs.org/'
+                    },
+                    {
+                        text: 'uniapp', link: 'https://uniapp.dcloud.io/'
+                    },
+                    {
+                        text: 'VuePress', link: 'https://vuepress.vuejs.org/zh/'
+                    },
+                    {
+                        text: 'vue-element-admin', link: 'https://panjiachen.github.io/vue-element-admin-site/zh/'
+                    },
+                    {
+                        text: 'D3', link: 'https://d3js.org/'
+                    },
+
+                    {
+                        text: 'element', link: 'http://element-cn.eleme.io/#/zh-CN'
+                    },
+                    {
+                        text: 'iView', link: 'https://www.iviewui.com/'
+                    },
+                    {
+                        text: 'TypeScript', link: 'https://www.tslang.cn/index.html'
+                    },
+                    {
+                        text: 'inMap', link: 'http://inmap.talkingdata.com/#/index'
+                    },
+                    {
+                        text: 'Koa', link: 'https://koa.bootcss.com/#context'
+                    },
+                    {
+                        text: 'mongodb', link: 'https://www.mongodb.com/'
+                    },
+                    {
+                        text: 'Valine', link: 'https://valine.js.org/'
+                    },
+                    {
+                        text: 'Weex', link: 'https://weex.apache.org/zh/'
+                    }
+                ]
             },
             {
-                text: 'npm 中文文档', link: 'https://www.npmjs.cn/'
-            },
-            {
-                text: 'Vue.js', link: 'https://cn.vuejs.org/'
-            },
-            {
-                text: 'Nuxt.js', link: 'https://zh.nuxtjs.org/'
-            },
-            {
-                text: 'uniapp', link: 'https://uniapp.dcloud.io/'
-            },
-            {
-                text: 'VuePress', link: 'https://vuepress.vuejs.org/zh/'
-            },
-            {
-                text: 'DCloud', link: 'http://www.dcloud.io/'
-            },
-            {
-                text: 'vue-element-admin', link: 'https://panjiachen.github.io/vue-element-admin-site/zh/'
-            },
-            {
-                text: 'D3', link: 'https://d3js.org/'
-            },
-            {
-                text: 'D2日报', link: 'https://daily.fairyever.com/'
-            },
-            {
-                text: 'element', link: 'http://element-cn.eleme.io/#/zh-CN'
-            },
-            {
-                text: 'iView', link: 'https://www.iviewui.com/'
-            },
-            {
-                text: '菜鸟教程', link: 'http://www.runoob.com/'
-            },
-            {
-                text: '前端库', link: 'https://www.awesomes.cn/'
-            },
-            {
-                text: 'TypeScript', link: 'https://www.tslang.cn/index.html'
-            },
-            {
-                text: 'inMap', link: 'http://inmap.talkingdata.com/#/index'
-            },
-            {
-                text: 'Koa', link: 'https://koa.bootcss.com/#context'
-            },
-            {
-                text: 'mongodb', link: 'https://www.mongodb.com/'
-            },
-            {
-                text: '阿里云', link: 'https://www.aliyun.com/?spm=a2c4e.11154921.1280361.1.668724dcivLtu3'
-            },
-            {
-                text: '百度地图', link: 'http://lbsyun.baidu.com/index.php?title=%E9%A6%96%E9%A1%B5'
-            },
-            {
-                text: 'ikcamp', link: 'https://www.ikcamp.com/'
-            },
-            {
-                text: 'Easy Mock', link: 'https://www.easy-mock.com/'
-            },
-            {
-                text: '码云', link: 'https://gitee.com/'
-            },
-            {
-                text: '图灵社区', link: 'http://www.ituring.com.cn/'
-            },
-            {
-                text: 'Valine', link: 'https://valine.js.org/'
-            },
-            {
-                text: 'Weex', link: 'https://weex.apache.org/zh/'
+                text: '学习社区',
+                items: [
+                    {
+                        text: 'MDN Web 文档', link: 'https://developer.mozilla.org/zh-CN/'
+                    },
+
+                    {
+                        text: 'DCloud', link: 'http://www.dcloud.io/'
+                    },
+                    {
+                        text: 'D2日报', link: 'https://daily.fairyever.com/'
+                    },
+                    {
+                        text: '菜鸟教程', link: 'http://www.runoob.com/'
+                    },
+                    {
+                        text: '前端库', link: 'https://www.awesomes.cn/'
+                    },
+                    {
+                        text: '阿里云', link: 'https://www.aliyun.com/?spm=a2c4e.11154921.1280361.1.668724dcivLtu3'
+                    },
+                    {
+                        text: '百度地图', link: 'http://lbsyun.baidu.com/index.php?title=%E9%A6%96%E9%A1%B5'
+                    },
+                    {
+                        text: 'ikcamp', link: 'https://www.ikcamp.com/'
+                    },
+                    {
+                        text: 'Easy Mock', link: 'https://www.easy-mock.com/'
+                    },
+                    {
+                        text: '码云', link: 'https://gitee.com/'
+                    },
+                    {
+                        text: '图灵社区', link: 'http://www.ituring.com.cn/'
+                    }
+                ]
             }
         ]
     },
