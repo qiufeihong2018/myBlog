477032674db216e5f9a367f1799a34766c672ddf
qiufeihong2018 <15058301288@163.com>
2020-04-11 20:29:51 +0800

    docs(《【设计】原子设计》):添加原子设计一文

--- docs/technical-translation/sidebar.js
+++ docs/technical-translation/sidebar.js
@@ -1,5 +1,11 @@
 module.exports = [
     {
+        title: '【设计】原子设计',
+        collapsable: false,
+        sidebarDepth: 4,
+        children: ['atomic-design/']
+    },
+    {
         title: '【react】prop-types包的一些说明',
         collapsable: false,
         sidebarDepth: 4,
